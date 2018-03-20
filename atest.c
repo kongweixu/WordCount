9d2664c9d213094ef5b17bfa704631a0c48c3adf
andy <479828125@qq.com>
2018-03-20 19:38:30 +0800

    first commit

---
+++ atest.c
@@ -0,0 +1,16 @@
+test()
+{
+File writename = new File()
+codeLine*/
+	BufferedeWriter out=new BufferedeWriter(new File);
+   //noteLine
+	out.write(outputBuffer);
+/*noteLine
+/*noteLine
+*/
+*/codeLine
+	out.flush();
+	out,close();
+}
+for(){
+}/*noteLine*/
